4188399984e6614c260c3e09d1801fba484c9766
leonschoene <lwdsch@gmx.de>
2023-10-01 14:02:14 +0200

    Initial Commit

---
+++ A2.py
@@ -0,0 +1,33 @@
+import ifcopenshell
+import ifcopenshell.util.element
+from pathlib import Path
+
+modelname = "LLYN - STRU"
+
+try:
+    dir_path = Path(__file__).parent
+    model_url = Path.joinpath(dir_path, 'model', modelname).with_suffix('.ifc')
+    model = ifcopenshell.open(model_url)
+except OSError:
+    try:
+        import bpy
+        model_url = Path.joinpath(Path(bpy.context.space_data.text.filepath).parent, 'model', modelname).with_suffix('.ifc')
+        model = ifcopenshell.open(model_url)
+    except OSError:
+        print(f"ERROR: please check your model folder : {model_url} does not exist")
+
+# Your script goes here
+
+# Test if everything works:
+spaces = model.by_type("IfcBeamType")
+for space in spaces:
+    print(space.HasPropertySets)
+
+
+beams = model.by_type('IfcBeam')
+columns = model.by_type('IfcColumn')
+slab = model.by_type('IfcSlab')
+
+print('beams: ', len(beams))
+print('columns: ', len(columns))
+print('slab: ', len(slab))
