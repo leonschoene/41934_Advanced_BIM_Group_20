5c54436a5643c2d81ed2779484e9bc3f2d6152f4
leonschoene <lwdsch@gmx.de>
2023-10-02 15:08:59 +0200

    changes with length

--- A2_Python_script.py
+++ A2_Python_script.py
@@ -48,6 +48,7 @@ def assign_nodes_to_elements(model):
 def get_nodes_from_geometry(element):
 # Here we have to analyze the geometry information of the element and extract the corresponding nodes. 
 # This could be complex and depends on the structure of our IFC file.
+# only for elements if load bearing is set to true?
 # Here is an example:
 
     nodes = []
@@ -75,19 +76,62 @@ def get_nodes_from_geometry(element):
 
 
 #3. part: create structural model
-if __name__ == '__main__':
-    ifc_file = model
+# if __name__ == '__main__':
+#     elements = assign_nodes_to_elements(model)
 
-    elements = assign_nodes_to_elements(ifc_file)
 
-elements = assign_nodes_to_elements(model)
+# Erstelle eine Liste, in der die Koordinaten jedes Beams gespeichert werden
+coordinates = []
 
-for element in elements:
-        print(f"Element ID: {element['element_id']}")
-        print("Nodes:")
-        for node in element['nodes']:
-            print(f"  - ({node['x']}, {node['y']}, {node['z']})")
-        print("\n")
+#Version 1
+# Gehe durch alle Beams in der IFC4-Datei
+# for beam in model.by_type("IfcBeam"):
+
+#     # Hole die Position des Anfangspunkts des Beams
+#     start_point = beam.ObjectPlacement.RelativePlacement.Location
+
+#     # Hole die Position des Endpunkts des Beams
+#     end_point = beam.ObjectPlacement.RelativePlacement.RefDirection
+
+#     # Wenn die Position des Endpunkts nicht `None` ist, dann füge sie der Liste hinzu
+#     if end_point is not None:
+#         coordinates.append((beam.Tag, start_point.Coordinates[0], start_point.Coordinates[1], start_point.Coordinates[2], end_point.DirectionRatios[0], end_point.DirectionRatios[1], end_point.DirectionRatios[2]))
+
+# print(coordinates)
+
+#Version 2
+# Gehe durch alle Beams in der IFC4-Datei
+# for beam in model.by_type("IfcBeam"):
+
+#     # Hole die Position des middlepoint des Beams
+#     middle_point = beam.ObjectPlacement.RelativePlacement.Location
+
+
+#     # Wenn die Position des Endpunkts nicht `None` ist, dann füge sie der Liste hinzu
+#     coordinates.append((beam.Tag, middle_point.Coordinates[0], middle_point.Coordinates[1], middle_point.Coordinates[2]))
+
+# print(coordinates)
+
+#Version 3
+# Gehe durch alle Beams in der IFC4-Datei
+for beam in model.by_type("IfcBeam"):
+
+    # Hole das Property Set des Beams
+    property_set = model.get_property_set_by_name("IfcBeamStandardCase")
+
+    # Hole die Länge aus dem Property Set
+    length = property_set.get("Length")
+
+    # Hole die Position des Beams
+    position = beam.Position
+
+    # Hole den Mittelpunkt des Beams
+    mid_point = position.Location
+
+    # Füge den Mittelpunkt und die Länge der Liste hinzu
+    coordinates.append((beam.Tag, mid_point.Coordinates[0], mid_point.Coordinates[1], mid_point.Coordinates[2], length))
+
+print(coordinates)
 
 # Test if everything works from teacher Martina: 
 # spaces = model.by_type("IfcBeamType")
