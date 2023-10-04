2c6184b611942a270ea8e7bbcb80e0819be4f39d
leonschoene <lwdsch@gmx.de>
2023-10-04 11:03:57 +0200

    got plane of beams

--- A2_Python_script.py
+++ A2_Python_script.py
@@ -1,9 +1,11 @@
 import ifcopenshell
 import ifcopenshell.geom
+import ifcopenshell.util.shape
 import ifcopenshell.util.element
 import ifcopenshell.util.placement
 import ifcopenshell.util.selector
 from pathlib import Path
+import numpy as np
 
 modelname = "LLYN - STRU"
 
@@ -94,20 +96,99 @@ def get_beam_values():
         z = ifcopenshell.util.selector.get_element_value(beam, 'z')
         slope = ifcopenshell.util.selector.get_element_value(beam, 'Pset_BeamCommon.Slope')
         length = ifcopenshell.util.selector.get_element_value(beam, 'Pset_BeamCommon.Span')
+        settings = ifcopenshell.geom.settings()
+        shape = ifcopenshell.geom.create_shape(settings, beam)
+        matrix = shape.transformation.matrix.data
+        matrix = ifcopenshell.util.shape.get_shape_matrix(shape)
         beam ={
             'Tag' : id,
             'X' : x,
             'Y' : y,
             'Z' : z,
             'Slope' : slope,
-            'Length' : length
+            'Length' : length,
+            'Matrix' : matrix
         }
         beam_values.append(beam)
 
     return beam_values
 
-beams = get_beam_values()
-print(beams)
+# beams = get_beam_values()
+# print(beams)
+
+# with open('liste.txt', 'w') as f:
+#     for element in beams:
+#         f.write(str(element) + '\n')
+
+beam = model.by_guid('1gWnGCa$TAouEuWitQnG8_')
+
+def get_beam_plane():
+    settings = ifcopenshell.geom.settings()
+    shape = ifcopenshell.geom.create_shape(settings, beam)
+    slope = ifcopenshell.util.selector.get_element_value(beam, 'Pset_BeamCommon.Slope')
+    verts = shape.geometry.verts
+    grouped_verts = ifcopenshell.util.shape.get_vertices(shape.geometry)
+
+    # Set all max differences of points equal 0
+    max_diff_x = 0
+    max_diff_y = 0
+    max_diff_z = 0
+    max_diff_index = None
+
+    # look for the biggest difference between nodes for each direction (x,y,z)
+    for i in range(1, len(grouped_verts)):
+        diff_x = abs(grouped_verts[i][0] - grouped_verts[i-1][0])
+        diff_y = abs(grouped_verts[i][1] - grouped_verts[i-1][1])
+        diff_z = abs(grouped_verts[i][2] - grouped_verts[i-1][2])
+
+        if diff_x > max_diff_x:
+            max_diff_x = diff_x
+        if diff_y > max_diff_y:
+            max_diff_y = diff_y
+        if diff_z > max_diff_z:
+            max_diff_z = diff_z
+
+    # look for the maximum difference between each axis 
+    max_diff_value = max(max_diff_x, max_diff_y, max_diff_z)
+
+    if slope == 0: # a horizontal beam has no second direction, it extend only in one axis
+        if max_diff_value == max_diff_x:
+            plane = 'x'
+        elif max_diff_value == max_diff_y:
+            plane = 'y'
+        else:
+            plane = 'z'
+        return plane
+    else: # if beam has a slope, it also has a second direction that is >> third direction which is his own depth
+        max_diff_list = sorted([max_diff_x, max_diff_y, max_diff_z], reverse=True) # sort the list 
+
+        first_max_diff = max_diff_list[0]
+        second_max_diff = max_diff_list[1]
+
+        # look which direction has the biggest difference
+        if first_max_diff == max_diff_x:
+            first_direction = "x"
+        elif first_max_diff == max_diff_y:
+            first_direction = "y"
+        else:
+            first_direction = "z"
+
+        # look which direction has the second biggest difference
+        if second_max_diff == max_diff_x:
+            second_direction = "x"
+        elif second_max_diff == max_diff_y:
+            second_direction = "y"
+        else:
+            second_direction = "z"
+
+        # create list of the plane and sort that alphabetical, so that we get xy-, xz-, or yz-plane
+        plane = [first_direction, second_direction]
+    
+    sorted_plane = sorted(plane)
+    return sorted_plane
+
+
+
 
 
 
